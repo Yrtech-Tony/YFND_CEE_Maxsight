dd10730ce112b4e34c4186914883379e33333c5d
Tony <moujunsheng15@163.com>
2018-02-05 20:54:27 +0800

    First Commit

---
+++ Web/INFI.Web/src/INFI.Web/Common/Module/SessionKeys.cs
@@ -0,0 +1,22 @@
+﻿namespace INFI.Web.Common.Module
+{
+    public static class SessionKeys
+    {
+        public const string SESSION_USERID = "infi_UserId";
+        public const string SESSION_USERNAME = "infi_UserName";
+        public const string SESSION_NAME = "infi_Name";
+        public const string SESSION_EMAIL = "infi_Email";
+        public const string SESSION_PHONE = "infi_Phone";
+        public const string SESSION_USERTYPE = "infi_UserType";
+        public const string SESSION_USERTYPENAME = "infi_UserTypeName";
+        public const string SESSION_DISNAME = "infi_DisName";
+        public const string SESSION_LOGGEDINAT = "infi_LoggedInAt";
+        public const string SESSION_ROLELIST = "infi_RoleList";
+        public const string SESSION_ZIONLIST = "infi_ZionList";
+        public const string SESSION_DEPARTMENTLIST = "infi_DepartmentList";
+        public const string SESSION_ORGZIONID = "infi_OrgZionId";
+        public const string SESSION_ORGAREAID = "infi_OrgAreaId";
+        public const string SESSION_ORGSERVERID = "infi_OrgServerId";
+        public const string SESSION_ORGDEPARTMENTID = "infi_OrgDepartmentId";
+    }
+}
---
+++ Web/INFI.Web/src/INFI.Web/Controllers/RecordController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using INFI.Web.Common;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace INFI.Web.Controllers
+{
+    public class RecordController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [PermissionRequired]
+        public IActionResult REC010()
+        {
+            DateTime now = DateTime.Now;
+            ViewBag.CurrentDate = now.ToString("yyyy-MM-dd");
+            ViewBag.FirstDay = new DateTime(now.Year, now.Month, 1).ToString("yyyy-MM-dd");
+            return View();
+        }
+
+        [PermissionRequired]
+        public IActionResult REC010P()
+        {
+            return View();
+        }
+    }
+}
