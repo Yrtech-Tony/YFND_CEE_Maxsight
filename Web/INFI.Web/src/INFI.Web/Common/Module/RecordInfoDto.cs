9e8121c46a2f68a8abb7c88e09200f2612cddc3a
huohaitao <huohaitao1@126.com>
2018-02-25 21:00:31 +0800

    Admin账户增加批量下载报备附件的功能

---
+++ Web/INFI.Web/src/INFI.Web/Common/Module/RecordInfoDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INFI.Web.Common.Module
+{
+    public class RecordInfoDto
+    {
+        public string RecordType { get; set; }
+        public string RecordTitle { get; set; }
+        public string ApprovalStatus { get; set; }
+        public string RecordDatetime { get; set; }
+        public string RecordUserName { get; set; }
+        public string ApprovalUserName { get; set; }
+        public string RecordReason { get; set; }
+        public string RecordUserId { get; set; }
+        public string ApprovalRemark { get; set; }
+        public List<AttachmentMngDto> AttachmentList { get; set; }
+        public List<AttachmentMngDto> ApproalAttachList { get; set; }
+    }
+}
