5500f5bac19145aefd61dc862e0817e933a371ff
Tony <moujunsheng15@163.com>
2018-08-23 22:56:14 +0800

    添加了文件

---
+++ API/src/INFI.API/Models/AppealMngDto/ChapterTypeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INFI.API.Models.AppealMngDto
+{
+
+    public class ChapterTypeDto
+    {
+       
+        public string ChapterTypeCode { get; set; }
+        public string ChapterTypeName { get; set; }
+    }
+
+
+}
